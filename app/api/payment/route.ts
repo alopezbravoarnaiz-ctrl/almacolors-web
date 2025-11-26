0a3a0a97ae65c6a913924d625095107e5501666c
Alejandro Lopez-Bravo <alejandrolopez-bravo@MacBook-Air-de-Alejandro.local>
2025-11-26 17:14:23 +0100

    Move checkout api to payment route to fix 405 errorv3

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -3,7 +3,8 @@ import Stripe from "stripe";
 
 // Inicializamos Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-11-20.acacia", 
+  // Mantén la fecha entre comillas, y pon 'as any' FUERA
+  apiVersion: "2024-11-20.acacia" as any, 
 });
 
 export async function POST(request: Request) {
